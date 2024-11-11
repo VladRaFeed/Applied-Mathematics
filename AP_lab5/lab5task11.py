f62b497574a326dfb40e3a2f29096f070cf967c9
VladRaFeed <122728801+VladRaFeed@users.noreply.github.com>
2024-11-11 07:12:38 +0200

    complete task 11

---
+++ AP_lab5/lab5task11.py
@@ -0,0 +1,8 @@
+from sympy import *
+
+t = Symbol('t')
+
+x = t**2 - 4*t
+y = ln(t) + sqrt(t)
+
+plot = plot_parametric((x, y, (t, 0.1, 5)), xlabel='x', ylabel='y', title='Параметрично задана крива')
